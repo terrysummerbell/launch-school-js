6549b68add9885e4b29aed64c3eb582ac8ad725d
Terry Summerbell <terrys@hey.com>
2021-04-02 22:24:35 +0100

    completed calculator refactor

--- js101/lesson2/calculator.js
+++ js101/lesson2/calculator.js
@@ -9,15 +9,36 @@ const readline = require('readline-sync');
 function prompt(message) {
   console.log(`=> ${message}`);
 }
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
 prompt('Welcome to Calculator!');
+
 prompt("What's the first number?");
 let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm...that doesn't appear to be a valid number");
+  number1 = readline.question();
+}
 
 prompt("What's the second number?");
 let number2 = readline.question();
 
+while (invalidNumber(number2)) {
+  prompt ("Hmm...that doesn't appear to be a valid number");
+  number2 = readline.question();
+}
+
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Please type 1, 2, 3 or 4");
+  operation = readline.question();
+}
 
 let output;
 switch (operation) {
